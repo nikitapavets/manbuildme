27c2f84fd593049eb0861fadae6b62f76b97f2d8
nikitapavets <nikitapavets@gmail.com>
2016-10-03 22:18:12 +0300

    user can change lang

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -2,10 +2,76 @@ angular.module('mainApp', ['ngStorage', 'ngRoute', 'dndLists'])
     .controller('mainController', function ($scope, $localStorage, $http) {
 
         $scope.$storage = $localStorage.$default({
-            styleColor: 'blue'
+            styleColor: 'blue',
+            lang: 'rus'
         });
 
         $scope.styleColor = $scope.$storage.styleColor;
+        $scope.lang = $scope.$storage.lang;
+        console.log($scope.lang);
+
+        $scope.$watch('lang', function(newValue, oldValue) {
+            $scope.$storage.lang = $scope.lang;
+            if(newValue == 'rus'){
+                $scope.ctext = $scope.ctex_rus;
+            }else{
+                $scope.ctext = $scope.ctex_eng;
+            }
+        });
+
+        $scope.ctex_rus = {
+            styles: 'Стиль',
+            lang: 'Язык',
+            top_pages: 'Популярные страницы',
+            last_pages: 'Последние обновления',
+            table_title: 'Название',
+            table_site: 'Сайт',
+            table_type: 'Тема',
+            table_rate: 'Рейтинг',
+            table_update: 'Обновление',
+            table_actions: 'Действия',
+            table_pages: 'Страницы',
+            table_created: 'Создание',
+            table_top: 'Топ',
+            style_red: 'Красный',
+            style_blue: 'Синий',
+            style_green: 'Зелёный',
+            lang_rus: 'Рус',
+            lang_eng: 'Анг',
+            tags_cloud: 'Облако тегов',
+            user_profile: 'Профиль',
+            user_exit: 'Выйти',
+            user_user: 'Пользователь',
+            user_settings: 'Настройки',
+            user_sites: 'Сайты',
+        };
+
+        $scope.ctex_eng = {
+            styles: 'Style',
+            lang: 'Lang',
+            top_pages: 'Top pages',
+            last_pages: 'Last pages',
+            table_title: 'Title',
+            table_site: 'Site',
+            table_type: 'Type',
+            table_rate: 'Rate',
+            table_update: 'Update',
+            table_actions: 'Actions',
+            table_pages: 'Pages',
+            table_created: 'Created',
+            table_top: 'Top',
+            style_red: 'Red',
+            style_blue: 'Blue',
+            style_green: 'Green',
+            lang_rus: 'Rus',
+            lang_eng: 'Eng',
+            tags_cloud: 'Tag cloud',
+            user_profile: 'Profile',
+            user_exit: 'Exit',
+            user_user: 'User',
+            user_settings: 'Settings',
+            user_sites: 'Sites',
+        };
 
         $scope.newColor = function(){
             $scope.$storage.styleColor = $scope.styleColor;
